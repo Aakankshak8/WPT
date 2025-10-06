cb04e0a45f49328350ad9cff1b9c6e58fc6aec74
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-10-07 00:00:16 +0530

    Upload assignments

---
+++ Day16/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Counttable from './countable';
+import Counter from './counter';
+import Stringli from './stringli';
+
+function App() {
+  return (
+    <div className="App">
+      <Counttable/>
+      <Counter/>
+      <Stringli/>
+      
+      
+    </div>
+  );
+}
+//
+export default App;
